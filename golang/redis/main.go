b57fd4e23cd7df345deda59294096e1223cf6d43
Francis Roch <francis@manafont.net>
2024-06-06 20:28:26 -0400

    Add golang redis queue

---
+++ golang/redis/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/redis/go-redis/v9"
+	"time"
+)
+
+var ctx = context.Background()
+
+func producer(rdb *redis.Client) {
+	for i := 0; i < 10; i++ {
+		item := fmt.Sprintf("item %d", i)
+		err := rdb.RPush(ctx, "queue", item).Err()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Produced", item)
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func consumer(rdb *redis.Client) {
+	for {
+		item, err := rdb.LPop(ctx, "queue").Result()
+		if err == redis.Nil {
+			break
+		} else if err != nil {
+			panic(err)
+		}
+		fmt.Println("Consumed", item)
+		time.Sleep(2 * time.Second)
+	}
+}
+
+func main() {
+	rdb := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+
+	go producer(rdb)
+	consumer(rdb)
+}
